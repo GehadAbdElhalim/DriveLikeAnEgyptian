3081c0ccabe45a76d69c66a732d75d367e8c3258
mekladious <miraekladious@gmail.com>
2018-05-07 11:27:38 +0200

    added upper view minimap

---
+++ test project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	Transform target;
+
+	void LateUpdate(){
+		try {
+			target = GameObject.Find ("Car(Clone)").transform;
+			transform.position = new Vector3 (target.position.x, transform.position.y, target.position.z);
+		} catch(System.Exception e){
+			Debug.LogException(e, this);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+//		target = GameObject.Find ("Car(Clone)").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
